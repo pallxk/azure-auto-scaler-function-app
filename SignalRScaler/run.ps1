302db8702d51df0a0882e9fbb5de8480d60b5792
pallxk <github@pallxk.com>
2021-11-08 11:04:46 +0800

    Allow specifying only resource group name

--- SignalRScaler/run.ps1
+++ SignalRScaler/run.ps1
@@ -3,46 +3,54 @@ param($Timer)
 
 $ErrorActionPreference = "Stop"
 
-# Variable definitions
-$resourceId = $env:SignalRResourceId
+# Get resources
+if ($env:SignalRResourceId) {
+    $signalRResources = @(Get-AzResource -ResourceId $env:SignalRResourceId)
+}
+elseif ($env:ResourceGroupName) {
+    $signalRResources = @(Get-AzSignalR -ResourceGroupName $env:ResourceGroupName)
+} else {
+    exit
+}
 
 $connectionsPerUnit = 1000          # Number of concurrent connections you can have per unit
 $unitCounts = 1,2,5,10,20,50,100    # Supported SignalR Unit Counts
 $scaleThreshold = [double]($env:SignalRScaleThreshold ?? .95)  # Percentage threshold at which to scale
 
-# Get information about the current resource state
-$signalRResource = Get-AzResource -ResourceId $resourceId
-$currentUnitCount = $signalRResource.Sku.Capacity
+foreach ($signalRResource in $signalRResources) {
+    # Get information about the current resource state
+    $currentUnitCount = $signalRResource.Sku.Capacity
 
-# Only scale if we are not on the Free Plan
-if ($signalRResource.Sku.Name.StartsWith("Free_")) {
-    Write-Host "Can't scale as resource is not on a scalable plan: " $signalRResource.Sku.Name
-    exit 1
-}
-
-# Get metrics for the last 5 minutes
-$connectionCountMetric = Get-AzMetric -ResourceId $resourceId -MetricName "ConnectionCount" -TimeGrain 00:05:00 -StartTime (Get-Date).AddMinutes(-5) -AggregationType Maximum
-$maxConnectionCount = $connectionCountMetric.Timeseries.Data[0].Maximum
-
-# Calculate the target unit count
-$targetUnitCount = 100
-foreach ($unitCount in $unitCounts) {
-    $unitCountConnections = $unitCount * $connectionsPerUnit
-    $unitCountConnectionsThreshold = $unitCountConnections * $scaleThreshold
-    if ($unitCountConnectionsThreshold -gt $maxConnectionCount) {
-        $targetUnitCount = $unitCount
-        Break
+    # Only scale if we are not on the Free Plan
+    if ($signalRResource.Sku.Name.StartsWith("Free_")) {
+        Write-Host "Can't scale as $($signalRResource.Name) is not on a scalable plan: $($signalRResource.Sku.Name)"
+        continue
     }
+
+    # Get metrics for the last 5 minutes
+    $connectionCountMetric = Get-AzMetric -ResourceId $signalRResource.Id -MetricName "ConnectionCount" -TimeGrain 00:05:00 -StartTime (Get-Date).AddMinutes(-5) -AggregationType Maximum
+    $maxConnectionCount = $connectionCountMetric.Timeseries.Data[0].Maximum
+
+    # Calculate the target unit count
+    $targetUnitCount = 100
+    foreach ($unitCount in $unitCounts) {
+        $unitCountConnections = $unitCount * $connectionsPerUnit
+        $unitCountConnectionsThreshold = $unitCountConnections * $scaleThreshold
+        if ($unitCountConnectionsThreshold -gt $maxConnectionCount) {
+            $targetUnitCount = $unitCount
+            Break
+        }
+    }
+
+    # See if we need to change the unit count
+    if ($targetUnitCount -eq $currentUnitCount) {
+        Write-Host "Not scaling as $($signalRResource.Name) is already at the optimum unit count: $currentUnitCount"
+        continue
+    }
+
+    Write-Host "Scaling $($signalRResource.Name) to unit count: $targetUnitCount"
+
+    # Change the resource unit count
+    $signalRResource.Sku.Capacity = $targetUnitCount
+    $signalRResource | Set-AzResource -Force
 }
-
-# See if we need to change the unit count
-if ($targetUnitCount -eq $currentUnitCount) {
-    Write-Host "Not scaling as resource is already at the optimum unit count: " $currentUnitCount
-    exit 0
-}
-
-Write-Host "Scaling resource to unit count: " $targetUnitCount
-
-# Change the resource unit count
-$signalRResource.Sku.Capacity = $targetUnitCount
-$signalRResource | Set-AzResource -Force
--- SqlDatabaseScaler/run.ps1
+++ SqlDatabaseScaler/run.ps1
@@ -3,37 +3,49 @@ param($Timer)
 
 $ErrorActionPreference = "Stop"
 
-# Variable definitions
-$resourceId = $env:SqlDatabaseResourceId
+# Get resources
+if ($env:SqlDatabaseResourceId) {
+    $sqlDatabaseResources = @(Get-AzResource -ResourceId $env:SqlDatabaseResourceId)
+}
+elseif ($env:ResourceGroupName) {
+    $sqlServerResources = @(Get-AzSqlServer -ResourceGroupName $env:ResourceGroupName)
+    foreach ($sqlServerResource in $sqlServerResources) {
+        $sqlDatabaseResources += @(Get-AzSqlDatabase -ResourceGroupName $env:ResourceGroupName -ServerName $sqlServerResource.ServerName)
+    }
+}
+else {
+    exit
+}
+
 [int]$storageScaleThreshold = ([double]$env:SqlDatabaseStorageScaleThreshold*100) ?? 95  # Percentage threshold at which to scale
 [long]$storageScaleStep= $env:SqlDatabaseStorageScaleStep ?? 10GB
 
-# Get information about the current resource state
-$sqlDatabaseResource = Get-AzResource -ResourceId $resourceId
+foreach ($sqlDatabaseResource in $sqlDatabaseResources) {
+    # Only scale if storage is scalable
+    if ($sqlDatabaseResource.Sku.Tier -in @("Basic", "Hyperscale")) {
+        Write-Host "Can't scale as $($sqlDatabaseResource.DatabaseName) is not on a scalable tier: $($sqlDatabaseResource.Sku.Tier)"
+        continue
+    }
 
-# Only scale if storage is scalable
-if ($sqlDatabaseResource.Sku.Tier -in @("Basic", "Hyperscale")) {
-    Write-Host "Can't scale as resource is not on a scalable tier: " $sqlDatabaseResource.Sku.Tier
-    exit 1
+    # Only scale if database is running
+    if ($sqlDatabaseResource.pausedDate) {
+        Write-Host "Not scaling as $($sqlDatabaseResource.DatabaseName) is paused"
+        continue
+    }
+
+    # Get metrics
+    $storagePercentMetric = Get-AzMetric -ResourceId $sqlDatabaseResource.ResourceId -MetricName "storage_percent" -TimeGrain 0:05:00 -StartTime (Get-Date).AddMinutes(-5) -AggregationType Maximum
+    $maxStoragePercent = $storagePercentMetric.Timeseries.Data[0].Maximum
+
+    Write-Host "Current storage usage of $($sqlDatabaseResource.DatabaseName): $maxStoragePercent% of $($sqlDatabaseResource.Properties.maxSizeBytes/1GB)GB"
+
+    # See if we need to scale
+    if ($maxStoragePercent -lt $storageScaleThreshold) {
+        Write-Host "Not scaling as $($sqlDatabaseResource.DatabaseName) is already at the optimum storage usage"
+        continue
+    }
+
+    $sqlDatabaseResource.Properties.maxSizeBytes += $storageScaleStep
+    Write-Host "Scaling $($sqlDatabaseResource.DatabaseName) to storage size: $($sqlDatabaseResource.Properties.maxSizeBytes/1GB)GB"
+    $sqlDatabaseResource | Set-AzResource -Force
 }
-
-# Only scale if database is running
-if ($sqlDatabaseResource.pausedDate) {
-    Write-Host "Not scaling as SQL database is paused"
-}
-
-# Get metrics
-$storagePercentMetric = Get-AzMetric -ResourceId $resourceId -MetricName "storage_percent" -TimeGrain 0:05:00 -StartTime (Get-Date).AddMinutes(-5) -AggregationType Maximum
-$maxStoragePercent = $storagePercentMetric.Timeseries.Data[0].Maximum
-
-Write-Host "Current storage usage: $maxStoragePercent% of $($sqlDatabaseResource.Properties.maxSizeBytes/1GB)GB"
-
-# See if we need to scale
-if ($maxStoragePercent -lt $storageScaleThreshold) {
-    Write-Host "Not scaling as resource is already at the optimum storage usage"
-    exit 0
-}
-
-$sqlDatabaseResource.Properties.maxSizeBytes += $storageScaleStep
-Write-Host "Scaling resource to storage size: $($sqlDatabaseResource.Properties.maxSizeBytes/1GB)GB"
-$sqlDatabaseResource | Set-AzResource -Force
